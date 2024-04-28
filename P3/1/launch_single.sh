9d5ac627fba9cd75e7ced29f59712dd01bd1ac96
agusSoria <51336992+agustin-Soria@users.noreply.github.com>
2024-04-28 18:58:55 -0300

    Create launch_single.sh

---
+++ P3/1/launch_single.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=gpgpu_practico
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --time=00:01:00
+
+#SBATCH --partition=besteffort
+# SBATCH --partition=normal
+
+#SBATCH --qos=besteffort_gpu
+# SBATCH --qos=gpu
+
+#SBATCH --gres=gpu:p100:1
+# #SBATCH --mail-type=ALL
+#SBATCH --mail-user=gabriel.kryger@fing.edu.uy
+#SBATCH -o consola.out
+
+export PATH=$PATH:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
+
+# Compilar el programa CUDA
+nvcc ej1a.cu -o ej1a.out
+
+# Ejecutar el programa CUDA con el archivo secreto.txt como argumento
+./ej1a.out secreto.txt
+
+
+
