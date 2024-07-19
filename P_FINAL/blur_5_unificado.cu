af98567f2e0917739ba66af07de3401717cda154
Gabriel Kryger <gabrielkrygermesa@gmail.com>
2024-07-18 23:40:15 -0300

    agregando 5 unificado

---
+++ P_FINAL/blur_5_unificado.cu
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "nvToolsExt.h"
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/sort.h>
+#include <thrust/sequence.h>
+#include <thrust/transform.h>
+
+#include <thrust/copy.h>
+#include <thrust/count.h>
+#include <thrust/functional.h>
+#include <cuda_runtime.h>
+#include <thrust/iterator/constant_iterator.h>
+#define MAX_DIGITS 32 // Assuming 32-bit integers
+#define MAX_INT 3000
+
+using namespace std;
+
+__global__ void gpuUnificado(int* img_in, int* windows, int width, int height, int W, int* img_out) {
+    int x = blockIdx.x;
+    int y = blockIdx.y;
+    int pixel = x + y * width;
+
+    int windowSize = (2 * W + 1) * (2 * W + 1);
+    extern __shared__ int sharedMem[];
+    int* output = sharedMem;
+    int* bitArray = &sharedMem[windowSize];
+    int* prefixSum = &sharedMem[2 * windowSize];
+    int* currentWindowCopy = &sharedMem[3 * windowSize];
+
+
+    if (pixel >= width * height) return;
+
+    int elemento_ventana = threadIdx.x + threadIdx.y * blockDim.x;
+
+    if (elemento_ventana >= windowSize) return;
+
+
+    int img_x = x + (threadIdx.x - W);
+    int img_y = y + (threadIdx.y - W);
+    int elemento_de_la_ventana_que_copiaremos = img_y * width + img_x;
+
+    if (img_x < 0 || img_x >= width || img_y < 0 || img_y >= height) {
+        currentWindowCopy[elemento_ventana] = 0;
+    } else {
+        currentWindowCopy[elemento_ventana] = img_in[elemento_de_la_ventana_que_copiaremos];
+    }
+
+    __syncthreads();
+
+    // Sort windows
+    if (x >= width || y >= height) return;
+    
+    int tdx = threadIdx.x + threadIdx.y * blockDim.x;
+    if (tdx >= windowSize) return;
+    
+    __shared__ int totalFalses;
+
+    for (int bit = 0; bit < MAX_DIGITS; bit++) {
+        int mask = 1 << bit;
+
+        // Extract bit
+        bitArray[tdx] = (currentWindowCopy[tdx] & mask) >> bit;
+        __syncthreads();
+
+        // Perform exclusive scan (prefix sum of not bit)
+        if (tdx == 0) {
+            prefixSum[0] = 0;
+            for (int i = 1; i < windowSize; i++) {
+                prefixSum[i] = prefixSum[i-1] + (1 - bitArray[i - 1]);
+            }
+            totalFalses = prefixSum[windowSize - 1] + (1 - bitArray[windowSize - 1]);
+        }
+        __syncthreads();
+    
+        // Reorder
+        int destination;
+        if (bitArray[tdx] == 0) {
+            destination = prefixSum[tdx];
+        } else {
+            destination = tdx - prefixSum[tdx] + totalFalses;
+        }
+        output[destination] = currentWindowCopy[tdx];
+        __syncthreads();
+
+        // Copy back to currentWindow
+        currentWindowCopy[tdx] = output[tdx];
+        __syncthreads();
+    }
+
+    // Select median
+    img_out[pixel] = currentWindowCopy[windowSize / 2];
+
+}
+
+void filtro_mediana_gpu(int* img_in, int* img_out, int width, int height, int W) {
+    int *d_img_in, *d_img_out, *d_windows;
+    size_t size = width * height * sizeof(int);
+    int windowSize = (2 * W + 1) * (2 * W + 1);
+
+    cudaMalloc(&d_img_in, size);
+    cudaMalloc(&d_img_out, size);
+    cudaMalloc(&d_windows, width * height * windowSize * sizeof(int));
+
+    cudaMemcpy(d_img_in, img_in, size, cudaMemcpyHostToDevice);
+
+    dim3 threadsPerBlock(2 * W + 1, 2 * W + 1);
+    dim3 blocksPerGrid(width, height);
+
+    // Fill windows
+    size_t sharedMemSize = 4 * windowSize * sizeof(int);
+    gpuUnificado<<<blocksPerGrid, threadsPerBlock, sharedMemSize>>>(d_img_in, d_windows, width, height, W, d_img_out);
+
+    cudaMemcpy(img_out, d_img_out, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_img_in);
+    cudaFree(d_img_out);
+    cudaFree(d_windows);
+
+    cudaError_t error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        printf("CUDA error: %s\n", cudaGetErrorString(error));
+    }
+}
+
+
--- P_FINAL/main_5.cu
+++ P_FINAL/main_5.cu
@@ -3,12 +3,12 @@
 #include <sys/time.h>
 #include "cuda.h"
 #include "cuda_runtime.h"
+#include "chrono"
 #include "CImg.h"
 #define ITERATIONS 10
 using namespace cimg_library;
 
 void filtro_mediana_gpu(int * img_in, int * img_out, int width, int height, int W);
-void filtro_mediana_cpu(int * img_in, int * img_out, int width, int height, int W);
 
 // Función para calcular la diferencia de tiempo en segundos
 double time_diff(struct timeval *start, struct timeval *end) {
@@ -16,8 +16,7 @@ double time_diff(struct timeval *start, struct timeval *end) {
 }
 
 int main(int argc, char** argv){
-
-
+	
 	const char * path;
 
 	if (argc < 2) printf("Debe ingresar el nombre del archivo\n");
@@ -28,31 +27,27 @@ int main(int argc, char** argv){
 	CImg<int> image_out(image.width(), image.height(),1,1,0);
 
 	int *img_matrix = image.data();
-   int *img_out_matrix = image_out.data();
+   	int *img_out_matrix = image_out.data();
 
 	int w = 3;
+
 	struct timeval start, end;
     gettimeofday(&start, NULL);
     for (int i = 0; i < ITERATIONS; i++){
-		// filtro_mediana_cpu(img_matrix, img_out_matrix, image.width(), image.height(), w);
+		filtro_mediana_gpu(img_matrix, img_out_matrix, image.width(), image.height(), w);
 	}
 	gettimeofday(&end, NULL);
 	double duration = time_diff(&start, &end);
 	
-	printf("version 1 ----------------------- \n");
-	printf("Quicksort \n");
-	printf("Tiempo CPU: %f\n", duration /ITERATIONS);
-	image_out.save("output_cpu_1.pgm");
-	
-	gettimeofday(&start, NULL);
-	for (int i = 0; i < ITERATIONS; i++){
-		filtro_mediana_gpu(img_matrix, img_out_matrix, image.width(), image.height(), w);
-	}
-	gettimeofday(&end, NULL);
-	duration = time_diff(&start, &end);
+	printf("version 5 ----------------------- \n");
+	printf("Paralelismo unificado: \n");
 	printf("Tiempo GPU: %f\n", duration /ITERATIONS);
-	image_out.save("output_gpu_1.pgm");
-   	
+	image_out.save("output_gpu_5.pgm");
+	
+
+
+	
+
    return 0;
 }
 
